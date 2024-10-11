9d5701e22d86f240c74463ccb94f02e3920b647b
Lojain Abdalrazaq <lojain.jalal@gmail.com>
2024-10-11 21:42:43 +0300

    changing the data dir

---
+++ assests/js/main.js
@@ -0,0 +1,20 @@
+import data from '../../data/data.json';
+
+/**
+ *  Fetch the data from the data.json file
+ */
+const fetchData = () => {
+    return data;
+};
+
+/**
+ *  Create single course html with given info
+ */
+const createCourse = (courseInfo) => {};
+
+/**
+ * 
+ */
+const createCourseList = (data) => {};
+// genrate the course on the HTML file
+const renderCourses = (data) => {}
